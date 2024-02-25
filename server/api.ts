83fa23f375a5bf44c0f2abd7b2a66e7e2685f68b
Rahzbob <tom.dejardin94@gmail.com>
2024-02-25 19:42:22 +0100

    Add backend API

--- server/api.ts
+++ server/api.ts
@@ -1,19 +1,22 @@
 import express, { Request, Response } from 'express';
-import pool from './dbconfig'; // Importer la configuration de la base de données depuis le dossier server
+import cors from 'cors';
+import pool from './dbconfig';
 
 const app = express();
 
-app.get('/api/data', async (req: Request, res: Response) => {
+app.use(cors());
+
+app.get('/api/countries', async (req: Request, res: Response) => {
   try {
-    const result = await pool.query('SELECT * FROM votre_table');
+    const result = await pool.query('SELECT country FROM statistics');
     res.json(result.rows);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Erreur serveur');
+    res.status(500).send('Server error');
   }
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Serveur en cours d'exécution sur le port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
