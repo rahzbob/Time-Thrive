820543d48994a2ed9895f5da4dcc70f4005043f6
Rahzbob <tom.dejardin94@gmail.com>
2024-03-31 22:00:48 +0200

    Add routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,5 +1,5 @@
 import { RouteRecordRaw } from 'vue-router';
-import LoginComponent from '../components/LoginComponent.vue';
+/* import LoginComponent from '../components/LoginComponent.vue';
 import SignUpComponent from '../components/SignUpComponent.vue';
 
 const routes: RouteRecordRaw[] = [
@@ -21,6 +21,23 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
+  },
+]; */
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/signin', component: () => import('pages/SignInPage.vue') },
+      { path: '/signup', component: () => import('pages/SignUpPage.vue') },
+      { path: '/results', component: () => import('pages/ResultsPage.vue') },
+      {
+        path: '/:catchAll(.*)*',
+        component: () => import('pages/ErrorNotFound.vue'),
+      },
+    ],
   },
 ];
 
