2bcb7651986d4f3dcedbe83d5c6f820f86b11261
Rahzbob <tom.dejardin94@gmail.com>
2024-04-05 12:23:47 +0200

    Add authentication in toolbar

---
+++ src/components/SignOutDialog.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-dialog>
+    <q-card>
+      <q-card-section class="row items-center q-pb-none">
+        <q-avatar icon="logout" />
+        <div class="text-h6">Sign out</div>
+        <q-space />
+        <q-btn icon="close" flat round dense v-close-popup />
+      </q-card-section>
+      <q-card-section class="row items-center">
+        <span class="q-ml-sm">You have successfully signed out.</span>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,14 +1,49 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
+      <q-toolbar class="row justify-between">
         <router-link to="/" style="text-decoration: none" class="text-white">
-          <q-toolbar-title> Time Thrive </q-toolbar-title>
+          <q-toolbar-title>Time Thrive</q-toolbar-title>
         </router-link>
+        <q-btn-dropdown unelevated rounded color="secondary" icon="person">
+          <q-list v-if="isAuthenticated">
+            <q-item @click="signOut" clickable v-ripple>
+              <q-item-section>Sign out</q-item-section>
+            </q-item>
+          </q-list>
+          <q-list v-else>
+            <q-item @click="router.push('/signin')" clickable v-ripple>
+              <q-item-section>Sign in</q-item-section>
+            </q-item>
+            <q-separator />
+            <q-item @click="router.push('/signup')" clickable v-ripple>
+              <q-item-section>Sign up</q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
       </q-toolbar>
     </q-header>
     <q-page-container>
       <router-view />
     </q-page-container>
+    <sign-out-dialog v-model="signOutAlert" />
   </q-layout>
 </template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import { useRouter } from 'vue-router';
+import { useUserStore } from 'src/stores/userStore';
+import SignOutDialog from 'src/components/SignOutDialog.vue';
+
+const router = useRouter();
+const userStore = useUserStore();
+const isAuthenticated = computed(() => userStore.authenticated);
+const signOutAlert = ref(false);
+
+async function signOut() {
+  signOutAlert.value = true;
+  localStorage.removeItem('token');
+  userStore.setAuthenticated(false);
+}
+</script>
